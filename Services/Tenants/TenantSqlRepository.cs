8f7b10d497ab75002006023395a189f2fe6371ef
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-04-06 23:35:06 -0300

    All sql repositories fully implement LoadEntity and UpdateFields methods

--- Services/Tenants/TenantSqlRepository.cs
+++ Services/Tenants/TenantSqlRepository.cs
@@ -11,9 +11,14 @@ public class TenantSqlRepository : EntitySqlRepository<Tenant>
     }
 
     protected override IQueryable<Tenant> LoadEntity(DbSet<Tenant> dbSet)
-        => dbSet.AsQueryable();
+        => dbSet.IncludePerson();
 
     protected override void UpdateFields(Tenant dbEntity, Tenant entity)
     {
+        dbEntity.Person.Address = entity.Person.Address;
+        dbEntity.Person.BirthDate = entity.Person.BirthDate;
+        dbEntity.Person.FirstName = entity.Person.FirstName;
+        dbEntity.Person.LastName = entity.Person.LastName;
+        dbEntity.Person.Mobile = entity.Person.Mobile;
     }
 }
